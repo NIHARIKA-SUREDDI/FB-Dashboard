59e85dabf983b0dceceb6b5da68e00416a6d1d3b
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-09 10:47:42 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,241 +2,70 @@ import logo from './logo.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { faTrash, faRotateRight, faCircleInfo ,faUser} from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faRotateRight, faCircleInfo ,faUser,faCheck} from '@fortawesome/free-solid-svg-icons';
 import products from './constants/products.json';
-import { faCheck, faCopy } from '@fortawesome/free-solid-svg-icons';
 import  { useState, useEffect } from 'react';
 import * as React from 'react';
-import Box from '@mui/material/Box'; 
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import TextField  from '@mui/material/TextField';
-  
-// function BasicSelect({ age, handleChange }) {
-//   return (
-//     <Box sx={{ minWidth: 150 }}>
-//       <FormControl fullWidth>
-//         <InputLabel id="demo-simple-select-label">State</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-label"
-//           id="demo-simple-select"
-//           value={age}
-//           label="Age"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value={10}>Todo</MenuItem>
-//           <MenuItem value={20}>Nurture</MenuItem>
-//           <MenuItem value={30}>Disqualified</MenuItem>
-//           <MenuItem value={30}>Inactive</MenuItem>
-//           <MenuItem value={30}>Upsell</MenuItem>
-//           <MenuItem value={30}>Won</MenuItem>
-//           <MenuItem value={30}>PaymentMissing</MenuItem>
-
-//         </Select>
-          
- 
-//       </FormControl>
-//     </Box>
-
-//   );
-// }
-
-// function BasicSelect1({ age1, handleChange }) {
-//   return (
-//     <Box sx={{ minWidth: 150 ,backgroundColor:'white'}}>
-//       <FormControl fullWidth>
-//         <InputLabel id="demo-simple-select-label">Quality </InputLabel>
-//         <Select
-//           labelId="demo-simple-select-label"
-//           id="demo-simple-select"
-//           value={age1}
-//           label="Age"
-//           onChange={handleChange}
-          
-//         >
-//           <MenuItem value={10}>Unsure</MenuItem>
-//           <MenuItem value={20}>Prem</MenuItem>
-//           <MenuItem value={30}>prof</MenuItem>
-//           <MenuItem value={30}>Team</MenuItem>
-//           <MenuItem value={30}>Spam</MenuItem>
-          
-//         </Select>
-          
-//       </FormControl>
-//     </Box>
-
-//   );
-// }
-
-// function BasicSelect2({ age2, handleChange }) {
-//   return (
-//     <Box sx={{ minWidth: 150 , backgroundColor:"white" }}>
-//       <FormControl fullWidth>
-//         <InputLabel id="demo-simple-select-label">Funnel Stage</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-label"
-//           id="demo-simple-select"
-//           value={age2}
-//           label="Age"
-//           onChange={handleChange}
-          
-        
-//         >
-//           <MenuItem value={10}>ToFu</MenuItem>
-//           <MenuItem value={20}>MoFu</MenuItem>
-//           <MenuItem value={30}>BoFu</MenuItem>
-//           <MenuItem value={30}>Closed</MenuItem>
-//           <MenuItem value={30}>InActive</MenuItem>
-
-          
-        
-          
-//         </Select>
-          
-//       </FormControl>    </Box>
-
-//   );
-//  }
+import { Box, FormControl, InputLabel, Select, MenuItem, TextField } from '@mui/material';
 
 
 
-
-
-
-
-
-function BasicSelect({ age, handleChange }) {
+function InputComponent({ type, label, value, handleChange, options = [] }) {
   return (
     <Box sx={{ minWidth: 150, backgroundColor: 'white' }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">State</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          label="State"
-          onChange={handleChange}
-          sx={{ backgroundColor: 'white', height: '55px', fontSize: '14px' }}
-        >
-          <MenuItem value={10}>Todo</MenuItem>
-          <MenuItem value={20}>Nurture</MenuItem>
-          <MenuItem value={30}>Disqualified</MenuItem>
-          <MenuItem value={40}>Inactive</MenuItem>
-          <MenuItem value={50}>Upsell</MenuItem>
-          <MenuItem value={60}>Won</MenuItem>
-          <MenuItem value={70}>Payment Missing</MenuItem>
-        </Select>
+        {type === 'select' ? (
+          <>
+            <InputLabel id={`${label}-label`}>{label}</InputLabel>
+            <Select
+              labelId={`${label}-label`}
+              id={`${label}-select`}
+              value={value}
+              label={label}
+              onChange={handleChange}
+              sx={{ backgroundColor: 'white', height: '55px', fontSize: '14px' }}
+            >
+              {options.map((option, idx) => (
+                <MenuItem key={idx} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </>
+        ) : (
+          <TextField
+            id={`${label}-text`}
+            value={value}
+            label={label}
+            onChange={handleChange}
+            sx={{
+              backgroundColor: 'white',
+              height: '50px',
+              fontSize: '14px',
+              lineHeight: '1.5',
+            }}
+          />
+        )}
       </FormControl>
     </Box>
   );
 }
-
-function BasicSelect1({ age1, handleChange }) {
-  return (
-    <Box sx={{ minWidth: 150, backgroundColor: 'white' }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Quality</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age1}
-          label="Quality"
-          onChange={handleChange}
-          sx={{ backgroundColor: 'white', height: '55px', fontSize: '14px' }}
-        >
-          <MenuItem value={10}>Unsure</MenuItem>
-          <MenuItem value={20}>Prem</MenuItem>
-          <MenuItem value={30}>Prof</MenuItem>
-          <MenuItem value={40}>Team</MenuItem>
-          <MenuItem value={50}>Spam</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
-}
-
-function BasicSelect2({ age2, handleChange }) {
-  return (
-    <Box sx={{ width: 150, backgroundColor: 'white' }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Funnel Stage</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age2}
-          label="Funnel Stage"
-          onChange={handleChange}
-          sx={{ backgroundColor: 'white', height: '55px', fontSize: '14px' }}
-        >
-          <MenuItem value={10}>ToFu</MenuItem>
-          <MenuItem value={20}>MoFu</MenuItem>
-          <MenuItem value={30}>BoFu</MenuItem>
-          <MenuItem value={40}>Closed</MenuItem>
-          <MenuItem value={50}>Inactive</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
-}
-
-function BasicSelect3({ age3, handleChange }) {
-  return (
-    <Box sx={{ width: 150, backgroundColor: 'white' }}>
-      <FormControl fullWidth>
-        <TextField
-          id="demo-simple-select"
-          value={age3}
-          label="Deal Size"
-          onChange={handleChange}
-          sx={{
-            backgroundColor: 'white',
-            height: '50px',
-            fontSize: '14px',
-            lineHeight: '1.5',
-          }}
-        />
-      </FormControl>
-    </Box>
-  );
-}
-
-function BasicSelect4({ age4, handleChange }) {
-  return (
-    <Box sx={{ width: 150, backgroundColor: 'white' }}>
-      <FormControl fullWidth>
-        <TextField
-          id="demo-simple-select"
-          value={age4}
-          label="Team Size"
-          onChange={handleChange}
-          sx={{
-            backgroundColor: 'white',
-            height: '50px',
-            lineHeight: '1.5',
-            fontSize: '14px',
-          }}
-        />
-      </FormControl>
-    </Box>
-  );
-}
-
-
-
 
 
 
 function App() {
   const [data, setData] = useState(null);
-  const [age, setAge] = useState('');
   const [isEditing, setIsEditing] = useState(false);
   const [isCopied, setIsCopied] = useState({});
-  const [age1,setAge1]=useState('');
-  const [age2,setAge2]=useState('');
-  const [age3,setAge3]=useState('');
-  const [age4,setAge4]=useState('');
+
+  const [formData, setFormData]  = React.useState({
+    state: '',
+    Quality: '',
+    funnelStage: '',
+    dealSize: '',
+    TeamSize: '',
+  });
+  
   const loanData = products.data[0]["i44ypDIeRFMXjLvALsKukCuxc3c2"];
 
   useEffect(() => {
@@ -260,27 +89,9 @@ function App() {
     alert("Data has been saved!"); 
   };
 
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleChange = (key) => (event) => {
+    setFormData({ ...formData, [key]: event.target.value });
   };
-
-  const handleChange1 = (event) => {
-    setAge1(event.target.value);
-  };
-  const handleChange2 = (event) => {
-    setAge2(event.target.value);
-  };
-  const handleChange3 = (event) => {
-    setAge3(event.target.value);
-  };
-  const handleChange4 = (event) => {
-    setAge4(event.target.value);
-  };
-
-
-
-
 
   const handleEdit = () => {
     setIsEditing(!isEditing); 
@@ -293,15 +104,11 @@ function App() {
     }
   };
 
-
   const handleRefresh = () => {
     setData(products); 
     alert("Data has been refreshed!");
   };
 
-  
-  
-  
 
   if (!data) {
     return <div>Loading...</div>;
@@ -312,9 +119,7 @@ function App() {
     const match = loanData.description.match(regex);
     return match ? match[1] : "N/A";
   };
-
-  
-  
+    
 
 
   return (
@@ -357,13 +162,8 @@ function App() {
         </p>
     </div>
 
-{/* 
-        <p>Country:{data.country_code || 'NA'}</p>
-        <p>Lender Type:{data.lenderType}</p>
-        <p>Price:{data.currency}</p>
-        {/* <p>Price: {data.price_in_purchased_currency} {data.currency}</p> */}
-        {/* <p>Last Seen:{new Date(data.last_seen).toLocaleString()}</p> */}
-        <div class="data-item">
+
+      <div class="data-item">
       <p class="value">Country </p>
       <p>{data.country_code || 'NA'}</p>
     </div>
@@ -374,14 +174,29 @@ function App() {
 
     <div class="data-item">
       <p class="value">Price</p>
-      <p class="label">{data.currency}</p>
+      {/* <p class="label">{data.price_in_purchased_currency
+      ||data.currency}</p> */}
+      <p class="label">{data.price_in_purchased_currency}</p>
     </div>
 
 
     <div class="data-item ">
-      <p class="value">Last Seen</p>
-      <p class="label">{new Date(data.last_seen).toLocaleString()}</p>
-      <p><span>{Math.floor((new Date() - new Date(data.last_seen)) / (1000 * 60 * 60 * 24))} days ago</span></p>
+    <p class="value">Last Seen</p>
+<p class="label">
+  {new Date(data.last_seen).toLocaleString([], {
+    year: 'numeric',
+    month: 'short', 
+    day: '2-digit', 
+    hour: '2-digit',
+    minute: '2-digit',
+  })}
+</p>
+<p>
+  <span>
+    {Math.floor((new Date() - new Date(data.last_seen)) / (1000 * 60 * 60 * 24))} days ago
+  </span>
+</p>
+
     </div>
        
       </div>
@@ -390,16 +205,61 @@ function App() {
       
 
       <div className="content">
-      <BasicSelect age={age} handleChange={handleChange} />
-      <BasicSelect1 age={age1} handleChange={handleChange1} />
-      <BasicSelect2 age={age2} handleChange={handleChange2} />
-      <BasicSelect3 age={age3} handleChange={handleChange3} />
-      <BasicSelect4 age={age4} handleChange={handleChange4} />
-      
+      <InputComponent
+          type="select"
+          label="State"
+          value={formData.age}
+          handleChange={handleChange("state")}
+          options={[
+            { value: 10, label: "Todo" },
+            { value: 20, label: "Nurture" },
+            { value: 30, label: "Disqualified" },
+            { value: 40, label: "Inactive" },
+            { value: 50, label: "Upsell" },
+            { value: 60, label: "Won" },
+            { value: 70, label: "Payment Missing" },
+          ]}
+        />
+        <InputComponent
+          type="select"
+          label="Quality"
+          value={formData.age1}
+          handleChange={handleChange("Quality")}
+          options={[
+            { value: 10, label: "Unsure" },
+            { value: 20, label: "Prem" },
+            { value: 30, label: "Prof" },
+            { value: 40, label: "Team" },
+            { value: 50, label: "Spam" },
+          ]}
+        />
+        <InputComponent
+          type="select"
+          label="Funnel Stage"
+          value={formData.age2}
+          handleChange={handleChange("funnelStage")}
+          options={[
+            { value: 10, label: "ToFu" },
+            { value: 20, label: "MoFu" },
+            { value: 30, label: "BoFu" },
+            { value: 40, label: "Closed" },
+            { value: 50, label: "Inactive" },
+          ]}
+        />
+        <InputComponent
+          type="text"
+          label="Deal Size"
+          value={formData.age3}
+          handleChange={handleChange("dealSize")}
+        />
+        <InputComponent
+          type="text"
+          label="Team Size"
+          value={formData.age4}
+          handleChange={handleChange("TeamSize")}
+        />
      
      <button  onClick={handleSave}>Save</button> 
-
-    
 
     </div>
 
@@ -440,14 +300,16 @@ function App() {
                 <p key={index}>
                  <p><li><b>Inquiry Purposes:</b></li></p>
                   <ul>
+
                     {personaDetails.inquiryPurpose.map((purpose, idx) => (
                       <li key={idx}>{purpose}</li>
                     ))}
                   </ul>
-                  <p><li><b>Controlled Access:</b> {personaDetails.giveControlledAccess ? 'true' : 'false'}</li></p>
-                  <p><li><b>Staff or Partners:</b>{personaDetails.haveStaffOrPartners ? 'true' : 'false'}</li></p>
-                  <p><li><b>lenderType:</b>{personaDetails.lenderType}</li></p>
-                  <p><li> <b>Number of Customers:</b> {personaDetails.numberOfCustomers}</li></p>
+                  <br></br>
+                  <p><li><b>Controlled Access: </b> {personaDetails.giveControlledAccess ? 'true' : 'false'}</li></p>
+                  {/* <p><li><b>Staff or Partners: </b>{personaDetails.haveStaffOrPartners ? 'true' : 'false'}</li></p> */}
+                  <p><li><b>lenderType: </b>{personaDetails.lenderType}</li></p>
+                  <p><li> <b>Number of Customers: </b> {personaDetails.numberOfCustomers}</li></p>
                 </p>
               );
             }
@@ -461,14 +323,12 @@ function App() {
 
     <div className="customer-section">
       <h4>Customer History  <FontAwesomeIcon icon={faCircleInfo} /></h4>
-  
-    
+
     <div className="custom-dropdown">
     <hr />
     </div>
     <div class="custom-dropdown-menu">
    
-
     <div class="custom-dropdown-item">
       <b>Subscription Type:</b>
       <div class="dropdown-content">
@@ -491,9 +351,12 @@ function App() {
     <div class="custom-dropdown-item">
       <b>Professional Subscription :</b>
       <div class="dropdown-content">
-        <span class="dropdown-time-ago">
-             Expires in {Math.floor((new Date(data.entitlements.professional.expires_date) - new Date()) / (1000 * 60 * 60 * 24))} days
-        </span>
+      
+     <span class="dropdown-time-ago">
+        Expires in {Math.abs(Math.ceil((new Date(data.entitlements.professional.expires_date) - new Date()) / (1000 * 60 * 60 * 24)))} days
+      </span>
+
+
         <span class="dropdown-date">
           ({new Date(data.entitlements.professional.expires_date).toLocaleDateString()})
         </span>
@@ -528,17 +391,11 @@ function App() {
 
   </div>
 </div>
-
-
-
-
          
     
     </div>
   );
 }
-
-
 
 
 export default App;
